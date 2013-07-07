8f37555eeca3fab661cb8d621ad351efdc05c0e3
M. Josenhans <m_josenhans@web.de>
2013-07-07 16:58:00 +0000

    Added buffer web.js

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var fs = require ('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buf = new Buffer("test");
+    var buf = new Buffer();
+    buf = readfile("index.html");
     response.send('Hello World from '+buf.toString());
 });
 
