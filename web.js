ad331d37d540f4e9d9027512ba48678ccc29506a
M. Josenhans <m_josenhans@web.de>
2013-07-07 16:34:19 +0000

    Read index.html in web.js

--- web.js
+++ web.js
@@ -1,10 +1,19 @@
 var express = require('express');
+var fs = require ('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var buf = new Buffer(readfile('index.html'));
+    response.send('Hello World from '+buf);
 });
+
+function readfile(filename){
+    fs.readFile(filename, function (err, data) {
+	if (err) throw err;
+	console.log(data);
+    });
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
